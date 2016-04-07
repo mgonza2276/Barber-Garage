89afcb31578983656b23841cbdc0eaa9dd5cbe6c
mgonza2276 <mygonzalez519@misena.edu.co>
2016-04-07 11:28:02 -0500

    Actualizacion
    Formularios de registro

--- View/agregar_usuario.php
+++ View/agregar_usuario.php
@@ -3,9 +3,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 	<head>
-	  <!--Aqui llamaremos los iconos que necesitaremos-->
-	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+	  <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="Materialize\materialize\css\materialize.css"  media="screen,projection"/>
@@ -28,7 +26,7 @@
     	</script>
 
     	<!--Aqui llamaremos los estilos necesarios-->
-	  	<link  rel="stylesheet" type="text/css" href="estilos.css">
+	  <link  rel="stylesheet" type="text/css" href="estilos.css">
 
 	</head>
 
@@ -39,18 +37,23 @@
 
       		<div class="row">
 
-      			<div id="formulario" class="col l6 offset-l3 " >
-					<form action="../Controller/usuarios.controller.php" method="POST">
-						<div class="col l12  input-field"  >
-							<center><h4>Registrar Usuario</h4></center>
+      			<div id="formulario" class="col l8 offset-l2">
+					<form action="../Controller/usuarios.controller.php" method="POST" >
+						<h3>Agregar Usuario</h3>
+						<div class="col l6  input-field"  >
+							<input type="hidden" name="pag" value="registro_usuario">
 							<i class="material-icons prefix">account_circle</i>
-							<input type="text" placeholder="Id_usuario..." name="id_usuario" required="" />
+							<input type="text" placeholder="Id_usuario..." name="id_usuario" required />
 							<i class="material-icons prefix">vpn_key</i>
 							<input type="password" placeholder="Clave..." name="clave" required/>
 							<i class="material-icons prefix">person_pin</i>
-							<input type="number" placeholder="Cedula..." name="cedula" required/>
+							<input type="number" placeholder="Cedula..." name="cedula"/>
 							<i class="material-icons prefix">person</i>
 							<input type="text" placeholder="Nombre..." name="nombre" />
+
+						</div>
+
+						<div class="col l6  input-field"  >
 							<i class="material-icons prefix">store</i>
 							<input type="text" placeholder="Dirección..." name="direccion" />
 							<i class="material-icons prefix">phone</i>
@@ -58,18 +61,28 @@
 							<i class="material-icons prefix">stay_current_portrait</i>
 							<input type="number" placeholder="Celular..." name="celular" id="icon_telephone"/>
 							<i class="material-icons prefix">email</i>
-							<input type="email" placeholder="Correo..." name="correo" required/>
-							<i class="material-icons prefix">assignment_ind</i> 
-							<div class="col l11 offset-l1">
-								<select id="select_perfil" name="perfil">
-									<option value="Administrador"> Administrador</option>
-									<option value="Empleado"> Empleado</option> 
-								</select>
-							</div>	
-							<button id="boton" class="waves-effect  btn-large green" type="submit" name="acc" value="c">Registrar</button>	
-							<button id="boton" class="waves-effect  btn-large red" type="submit"   >Cancelar</button>
+							<input type="email" placeholder="Correo..." name="correo" required />
+						</div>
+						
+						<div class="row">
+							<div class="col l6">
+									<select id="select_perfil" name="perfil">
+										<option value="Administrador"> Administrador</option>
+										<option value="Empleado"> Empleado</option> 
+									</select>
+							</div>
+						</div>
+						<div class="col l6  input-field">
+							<button id="boton" class="waves-effect  btn-large cyan" name="acc" value="c" >Registrar</button>
+						</div>
+						<div class="col l6  input-field">
+							<a id="boton" href="../Components/footer.php" class="waves-effect  btn-large  blue-grey darken-1">Cancelar</a>
+						</div>
+
+							
 							<?php echo @$_REQUEST["msn"]; ?>
 						</div>
+
 					</form>			
 				</div>
 			</div>
