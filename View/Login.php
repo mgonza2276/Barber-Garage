181795ba9763bb8756ccef36484ad13183b1cd8a
villadac <ccvillada4@misena.edu.co>
2016-04-13 06:30:54 -0500

    a.

--- View/Login.php
+++ View/Login.php
@@ -17,26 +17,38 @@
 
     <link  rel="stylesheet" type="text/css" href="estilos.css">
 </head>
-<body id="fondo">
+<body class="fondo">
+
+  <?php 
+    include_once("../Components/menu.php")
+   ?>
+
 	<div class="container">
       	<div class="row">
-      		<div id="centro"  class="col l4 offset-l4 input-field" >
+      		<div id="centro"  class="col l4 s12 offset-l4 input-field" >
       			<form>
-      				<center><h4>Login</h4></center>
+      			<center><h4>Login</h4></center>
 						<i class="material-icons prefix">account_circle</i>
-						<input type="text" placeholder="Id_usuario..." name="id_usuario" required />
+						  <input type="text" placeholder="Id_usuario..." name="id_usuario" required />
 
 						<i class="material-icons prefix">vpn_key</i>
-						<input type="password" placeholder="Contraseña..." name="clave" required/>
+						  <input type="password" placeholder="Contraseña..." name="clave" required/>
 
-						<button id="boton" class="waves-effect  btn-large green" type="submit" name="" value="">Ingresar</button>
+						<button id="boton" class="waves-effect btn-large green" type="submit" name="" value="" href="#">Ingresar</button> 
 
-						<a href="">Recuperar Contraseña</a>	
-							
-      			</form>
+            <div class="col l5 s12">
+                <input type="checkbox" class="filled-in" id="filled-in-box" checked="checked" />
+                <label for="filled-in-box">Recordar</label>              
+              </div>   
+            <div class="col l7 s12" id="lgn-rec">
+              <a  href="">Recuperar Contraseña</a>
+            </div>         
+            </form>
       		</div>
     	</div>
     </div>
-
+<?php 
+    include_once("../Components/footer.php")
+   ?>
 </body>
 </html>
