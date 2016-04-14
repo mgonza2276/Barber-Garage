8a02c1dfb214e1a73ce37312e035f9f9d6147030
Santiago Hoyos <shoyos30@misena.edu.co>
2016-04-14 11:45:32 -0500

    buscar barberia
    menus

---
+++ View/buscar_barberia.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>buscar_barberia</title>
+	<meta charset="utf-8"/>
+	<!--Aqui llamaremos los iconos que necesitaremos-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="Materialize\materialize\css\materialize.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="Materialize\jquery-1.12.1.min.js"></script>
+    <script type="text/javascript" src="Materialize\materialize\js\materialize.js"></script>
+
+    <link  rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	<?php
+		include_once("../Components/menu_admin.php") 
+			
+	?>
+	<?php
+		include_once("../Components/footer.php")
+	?>
+</body>
+</html>
