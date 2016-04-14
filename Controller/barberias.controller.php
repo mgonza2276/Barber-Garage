25a22a8771c95b043be6142b4bbf835a59eb12cc
villadac <criss.vill@hotmail.com>
2016-04-14 10:16:42 -0500

    cambio de name nit

--- Controller/barberias.controller.php
+++ Controller/barberias.controller.php
@@ -2,7 +2,7 @@
 	//Llamamos la conexion a la base de datos
 	require_once("../Model/conexion.php");
 	BarberGarage_BD::Connect();
-	
+
 	//Llamamos las clases que necesitamos
 	require_once("../Model/barberias.class.php");
 
@@ -12,12 +12,12 @@
 	case 'c':
 		# crear
 		#iniciamos las variables   que se envian desde el  formulario  y las  que necesito  para  almacenar la tabla.
-	    $Cod_barberia  	=$_POST["Nit"];
-		$Nombre      	=$_POST["nombre"];  
-		$Direccion   	=$_POST["direccion"]; 
+	  $Cod_barberia  	=$_POST["nit"];
+		$Nombre      	=$_POST["nombre"];
+		$Direccion   	=$_POST["direccion"];
 		$Telefono    	=$_POST["telefono"];
 		$Ciudad      	=$_POST["ciudad"];
-		
+
 
 		try {
 			Gestion_barberias::Create($Cod_barberia,$Nombre,$Direccion,$Telefono,$Ciudad);
@@ -25,12 +25,12 @@
 			} catch (Exception $e) {
 				$mensaje=":( ha  ocurrido un error, el error  fue: ".$e->getMessage()." en ".$e->getFile(). " en la linea".$e->getLine();
 			}
-		
+
 		echo "Registro barberia exitoso";
 		header("location: ../View/Registrar_barberia.php?msn=".$mensaje);
 
 		break;
-			 
+
 		}
 
 ?>
